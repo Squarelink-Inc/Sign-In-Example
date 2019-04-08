3b699dea1dfd745e21ea3b5e53f5fb886a2dae4a
Alexander Patin <alex@squarelink.com>
2019-04-08 17:34:17 -0400

    updated client_id

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,5 +1,5 @@
 export default {
-  CLIENT_ID: 'f11116f2b57b4f481d951dd6b373e6',
+  CLIENT_ID: 'f11116f2b57b4f481d95',
   REDIRECT: 'https://squarelink-inc.github.io/Sign-In-Example',
   APP_URL: 'https://app.squarelink.com',
   API_URL: 'https://api.squarelink.com'
